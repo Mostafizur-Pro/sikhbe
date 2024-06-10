7dd2cec1e140c4b755fc7c94de3d6a2768dac0a9
mostafizur-pro <mostafizur0195@gmail.com>
2024-06-10 18:01:09 +0600

    update home page

--- src/components/HomePage/TechnologyLearn.tsx
+++ src/components/HomePage/TechnologyLearn.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const CourseDetails = () => {
+const TechnologyLearn = () => {
   return (
     <div>
       <div className="text-center py-10">
@@ -51,53 +51,8 @@ const CourseDetails = () => {
           </p>
         </div>
       </div>
-
-      <div className="flex justify-center m-10 pb-40">
-        <div className="relative inline-block w-auto col-span-1">
-          <div className="">
-            <img
-              className="object-cover w-full h-full"
-              decoding="async"
-              loading="lazy"
-              src="https://bonx-react.pages.dev/static/40259a30cb4b2bf6d1b9877053fa1183/0a8d7/gaming-bg1.webp"
-              alt="Background"
-            />
-          </div>
-
-          <div className="absolute inset-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <span
-              className="relative w-14 h-14 md:w-24 md:h-24 lg:w-28 lg:h-28 inline-block cursor-pointer"
-              role="button"
-            >
-              <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-purple-100 opacity-50"></span>
-              <img
-                className="relative w-14 h-14 md:w-24 md:h-24 lg:w-28 lg:h-28"
-                decoding="async"
-                loading="lazy"
-                src="https://bonx-react.pages.dev/static/c1a7a32d0b562b2ea62f64765a3e8213/1db2c/play-btn.webp"
-                alt="Play button"
-              />
-            </span>
-          </div>
-
-          <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-10">
-            <div className="relative w-80 max-w-full mx-auto">
-              <img
-                className="object-cover w-full h-full"
-                decoding="async"
-                loading="lazy"
-                src="https://bonx-react.pages.dev/static/d10045891c4b4a87e886c94db720fede/48f4c/text-mini-shape.webp"
-                alt="Text background"
-              />
-              <h3 className="absolute inset-0 flex items-center justify-center text-white text-xl md:text-2xl lg:text-3xl font-bold uppercase">
-                Watch Video
-              </h3>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
 
-export default CourseDetails;
+export default TechnologyLearn;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HomePage/HeroSection";
 import HeroFAQ from "@/components/HomePage/HeroFAQ";
+import TechnologyLearn from "@/components/HomePage/TechnologyLearn";
 
 const Home = () => {
   return (
     <div className="bg-main text-white">
       <HeroSection />
+      <TechnologyLearn />
       <div className="pb-10">
         <HeroFAQ />
       </div>
